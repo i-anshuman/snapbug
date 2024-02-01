836c362012cc0572ab6b02179ff36a0e714b254d
Anshuman <anshuman844@gmail.com>
2024-02-01 23:38:35 +0530

    feat: Create IssueCreationDTO and add validation messages
    - Added the IssueCreationDTO class to represent the data transfer object for creating issues.
    - Implemented validation annotations in the IssueCreationDTO class to ensure data integrity and consistency.
    - Added corresponding validation messages in the messages.properties file to provide meaningful error messages for validation failures.
    - These changes improve the robustness and reliability of input data validation in the system, enhancing overall data quality.

---
+++ src/main/java/com/snapbug/dtos/IssueCreationDTO.java
@@ -0,0 +1,39 @@
+package com.snapbug.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@Builder
+@ToString(doNotUseGetters = true)
+public class IssueCreationDTO {
+
+  @NotBlank(message = "{validation.issue.title}")
+  private String title;
+
+  @NotBlank(message = "{validation.issue.description}")
+  private String description;
+
+  @NotBlank(message = "{validation.issue.expectation}")
+  private String expectation;
+
+  @NotNull(message = "{validation.issue.module}")
+  @Positive(message = "{validation.issue.module.valid}")
+  private Long module;
+
+  @NotNull(message = "{validation.issue.submodule}")
+  @Positive(message = "{validation.issue.submodule.valid}")
+  private Long submodule;
+
+  @NotNull(message = "{validation.issue.screen}")
+  @Positive(message = "{validation.issue.screen.valid}")
+  private Long screen;
+
+  @NotNull(message = "{validation.issue.type}")
+  @Positive(message = "{validation.issue.type.valid}")
+  private Long type;
+}
