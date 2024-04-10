edbeda0884659befe14bb931104dc1683287f2df
Anshuman <anshuman844@gmail.com>
2024-04-11 00:54:12 +0530

    feat: Enable method security

--- src/main/java/com/snapbug/configs/SecurityConfiguration.java
+++ src/main/java/com/snapbug/configs/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,6 +25,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
   private final AuthenticationEntryPoint authenticationEntryPoint;
